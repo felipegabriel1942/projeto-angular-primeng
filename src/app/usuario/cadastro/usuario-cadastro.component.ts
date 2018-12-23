f241f6a21bdcf9e5794241f88b9808c3a8a06eb6
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-12-23 16:24:24 -0300

    Formulario simples

---
+++ src/app/usuario/cadastro/usuario-cadastro.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/usuario';
+
+
+@Component({
+  selector: 'app-usuario-cadastro',
+  templateUrl: './usuario-cadastro.component.html'
+})
+export class UsuarioCadastroComponent implements OnInit {
+
+
+    public usuario:Usuario = new Usuario()
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  public salvar(){
+      console.log(this.usuario)
+      alert("Salvo com sucesso!")
+  }
+
+}
